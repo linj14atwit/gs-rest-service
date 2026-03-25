a1030e213a89f23908c1347018b7f486f20c5873
jamlin416 <jamlin45522045@gmail.com>
2026-03-25 03:47:52 -0400

    employee controller from example

---
+++ complete/src/main/java/com/example/restservice/EmployeeController.java
@@ -0,0 +1,28 @@
+//Example provide by Hewlett Packard Enterprise Development Job Simulation
+
+package com.example.restservice;
+  
+import java.net.URI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.restservice.Employees;
+import com.example.restservice.EmployeeManager;
+  
+// Creating the REST controller
+@RestController
+@RequestMapping(path = "/employees")
+public class EmployeeController {
+  
+    @Autowired
+    private EmployeeManager employeeManager;
+
+    @GetMapping(path = "/", produces = "application/json")
+    public Employees getEmployees()
+    {
+        return employeeManager.getAllEmployees();
+    }
+
+}
